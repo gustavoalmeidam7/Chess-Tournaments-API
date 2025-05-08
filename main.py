38202710c324e39bf28ea3cb97937d0f7a8b110a
Emerson <79228789+OEsqueleto@users.noreply.github.com>
2025-05-08 00:36:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 from fastapi import FastAPI
 from noticias_api import router as noticias_router
 from comunicados_api import router as comunicados_router
-from jogadores_api import router as jogadores_router
-from torneios_api import router as torneios_router
+#from jogadores_api import router as jogadores_router
+#from torneios_api import router as torneios_router
 
 # Informações básicas
 app = FastAPI(
@@ -16,5 +16,5 @@ app = FastAPI(
 # Inclui cada sub-API
 app.include_router(noticias_router)
 app.include_router(comunicados_router)
-app.include_router(jogadores_router)
-app.include_router(torneios_router)
+#app.include_router(jogadores_router)
+#app.include_router(torneios_router)
